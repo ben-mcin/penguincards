466563e68e76ee08cb987628a9a0cf278c9439a9
tnoves <ben.mcintyre10@gmail.com>
2026-09-13 19:52:29 +0100

    defined card class

---
+++ card.py
@@ -0,0 +1,47 @@
+from dataclasses import dataclass
+from enum import Enum
+
+
+class Element(Enum):
+    FIRE = "🔥"
+    WATER = "💧"
+    SNOW = "❄️ " #Extra space added for print formatting problems
+
+
+class Colour(Enum):
+    RED = "red"
+    BLUE = "blue"
+    YELLOW = "yellow"
+    GREEN = "green"
+    ORANGE = "orange"
+    PURPLE = "purple"
+
+
+class Power(Enum):
+    #TODO implement powers
+    PLACEHOLDER = ""
+
+
+
+@dataclass(frozen=True)
+class Card:
+    element: Element
+    number: int
+    colour: Colour
+    power: Power | None = None
+
+    def __str__(self):
+        return f"{self.element.value} {self.number} {self.colour.value}"
+    
+    def __repr__(self):
+        return str(self)
+
+    # Card creation check, can be removed once cards are defined
+    def __post_init__(self):
+        if not 1 <= self.number <= 12:
+            raise ValueError("Card number must be between 1 and 12")
+        if self.number >= 10 and self.power is None:
+            raise ValueError("Cards numbered 10-12 must have a power")
+        if self.number < 10 and self.power is not None:
+            raise ValueError("Cards numbered 1-9 cannot have a power")
+        
