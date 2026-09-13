d8522bcd78b159fb43753b6b1bf447849b51306b
tnoves <ben.mcintyre10@gmail.com>
2026-09-13 20:41:57 +0100

    added player class with placeholders

---
+++ player.py
@@ -0,0 +1,36 @@
+from deck import Deck
+import random
+
+class Player:
+    def __init__(self, deck):
+        self.player_deck = deck.cards.copy()
+        random.shuffle(self.player_deck)
+        self.hand = []
+        self.graveyard = []
+        self.wins = []
+
+    # Shuffle player deck
+    def shuffle_deck(self):
+        self.player_deck.shuffle()
+
+    # Draw card from deck
+    def draw_card(self):
+        self.hand.append(self.player_deck.pop(0))
+
+    # Get cards in hand
+    def get_hand(self):
+        return self.hand
+
+    def get_player_deck(self):
+        return self.player_deck
+
+    #TODO recycle cards from graveyard into deck and shuffle
+    def recycle_graveyard(self):
+        return None
+    
+    #TODO allow player to select card from hand to play
+    def play_card(self, card=None):
+        card = self.hand.pop()
+        self.graveyard.append(card)
+        return card
+
