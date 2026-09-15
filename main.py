9d74f0058a252bdb3e23d1e41f3f27b97345c6a5
tnoves <ben.mcintyre10@gmail.com>
2026-09-15 10:05:34 +0100

    improved game loop structure

--- main.py
+++ main.py
@@ -18,36 +18,36 @@ def determine_winner(card1: Card, card2: Card) -> str:
 
     return 1 if ELEMENT_WINS_AGAINST[card1.element] == card2.element else 2
 
-
+# Initialise decks and players
 player_deck = Deck()
 computer_deck = Deck()
 
 player = Player(player_deck)
 computer = Player(computer_deck)
 
-# Initial setup
-player.draw_card()
-player.draw_card()
-player.draw_card()
-player.draw_card()
-player.draw_card()
+# Main game loop
+while True:
+    # Draw cards until both players have 5 cards in hand
+    while len(player.get_hand()) < 5:
+        player.draw_card()
+    while len(computer.get_hand()) < 5:
+        computer.draw_card()
 
-computer.draw_card()
-computer.draw_card()
-computer.draw_card()
-computer.draw_card()
-computer.draw_card()
+    # Play cards
+    player_card = player.play_card()
+    computer_card = computer.play_card()
 
-player_card = player.play_card()
-computer_card = computer.play_card()
-print(f"{player_card} vs {computer_card}")
+    print(f"Player plays: {player_card}\nComputer plays: {computer_card}")
 
-match determine_winner(player_card, computer_card):
-    case 0:
-        print("Draw")
-    case 1:
-        print("Player wins")
-        player.add_win(player_card)
-    case 2:
-        print("Computer wins")
-        computer.add_win(computer_card)
+    # Determine winning hand and add to players win conditions
+    match determine_winner(player_card, computer_card):
+        case 0:
+            print("Draw")
+        case 1:
+            print("Player wins")
+            player.add_win(player_card)
+        case 2:
+            print("Computer wins")
+            computer.add_win(computer_card)
+
+    break
