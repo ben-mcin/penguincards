deb5158671536f493267cc809243f0784489a529
tnoves <ben.mcintyre10@gmail.com>
2026-09-15 21:17:13 +0100

    implement normal difficulty that plays defensively when available

--- cpu.py
+++ cpu.py
@@ -16,6 +16,20 @@ def evaluate_card(card, wins):
     temp_wins[card.element.value].add(card.colour.value)
     return evaluate_win_condition(temp_wins)
 
+def evaluate_opponent(wins):
+    scores = {"element_win": sum(bool(items) for items in wins.values()),
+              "🔥": len(wins["🔥"]),
+              "❄️": len(wins["❄️"]),
+              "💧": len(wins["💧"])}
+    return scores
+
+
+AVOID_ELEMENTS = {
+    "🔥": "❄️",
+    "❄️": "💧",
+    "💧": "🔥",
+}
+
 
 class CPU(Player):
     def __init__(self, deck, difficulty="dumb"):
@@ -23,17 +37,17 @@ class CPU(Player):
 
         self.difficulty = difficulty
 
-        if difficulty == "normal":
+        if difficulty == "easy":
+            self.play_card = self._play_card_easy
+        elif difficulty == "normal":
             self.play_card = self._play_card_normal
-        elif difficulty == "hard":
-            self.play_card = self._play_card_hard
         elif difficulty == "dumb":
             pass
         else:
             raise ValueError("Invalid difficulty level. Choose from 'dumb', 'normal', or 'hard'.")
 
     # Implements normal difficulty where CPU makes decision based on it's own win conditions.
-    def _play_card_normal(self, opponent_wins):
+    def _play_card_easy(self, opponent_wins):
         # Gets weighting for each card in hand based on how it affects the CPU's win conditions
         scores = []
         for card in self.hand:
@@ -50,7 +64,38 @@ class CPU(Player):
         
         return super().play_card(self.hand.index(result[1]))
 
-    # TODO implement smarter card selection for CPU
-    def _play_card_hard(self, opponent_wins):
-        # Play a random card from hand
-        return super().play_card()
+    # Implements normal difficulty where CPU plays based on opponents win conditions, falling back on easy difficulty if none available
+    def _play_card_normal(self, opponent_wins):
+        likely_choices = set()
+        weighted_choices = evaluate_opponent(opponent_wins)
+        if weighted_choices.get("element_win") == max(v for k, v in weighted_choices.items()):
+            for k, v in weighted_choices.items():
+                if v == 0 and k != "element_win":
+                    likely_choices.add(k)
+        for k, v in weighted_choices.items():
+            if k != "element_win" and v == max(v for k, v in weighted_choices.items()):
+                if k != "element_win":
+                    likely_choices.add(k)
+        avoid = {AVOID_ELEMENTS[x] for x in likely_choices}
+
+        scores = []
+        for card in self.hand:
+            if card.element.value in avoid:
+                print(f"avoiding {card}")
+                pass
+            else:
+                score = evaluate_card(card, self.wins)
+                scores.append((score, card))
+
+        if len(scores) > 0:
+            best_score, best_number = max((score, card.number) for score, card in scores)
+            result = random.choice([
+                        (score, card)
+                        for score, card in scores
+                        if (score, card.number) == (best_score, best_number)
+                    ])
+            return super().play_card(self.hand.index(result[1]))
+        else:
+            print("no defensive choice")
+            return self._play_card_easy(opponent_wins)
+    
