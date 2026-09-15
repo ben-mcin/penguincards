1e6bf69697ebd6c33ed982b6444800809206f4ac
tnoves <ben.mcintyre10@gmail.com>
2026-09-15 09:51:09 +0100

    improved drawing cards and implemented recylcling graveyard back into deck

--- player.py
+++ player.py
@@ -15,9 +15,12 @@ class Player:
     def shuffle_deck(self):
         random.shuffle(self.player_deck)
 
-    # Draw card from deck
-    def draw_card(self):
-        self.hand.append(self.player_deck.pop(0))
+    # Draw card from deck, recycles graveyard if deck is empty, default draws 1 card
+    def draw_card(self, count: int = 1):
+        if not self.play_deck:
+            self.recycle_graveyard()
+        for x in range(0, count):
+            self.hand.append(self.player_deck.pop(0))
 
     # Get cards in hand
     def get_hand(self):
@@ -26,9 +29,11 @@ class Player:
     def get_player_deck(self):
         return self.player_deck
 
-    #TODO recycle cards from graveyard into deck and shuffle
+    # Shuffle graveyard back into deck
     def recycle_graveyard(self):
-        return None
+        self.player_deck += self.graveyard
+        self.graveyard = []
+        self.shuffle_deck()
     
     #TODO allow player to select card from hand to play
     def play_card(self, card=None):
